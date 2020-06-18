e8cd7d7de70f9c3b2164a71dbe083d19db8c5af7
Administrator <Administrator@WIN-P8CJ2BN5V2H>
2020-06-18 23:39:42 +0530

    commit

---
+++ prog1/src/com/wipro/languagebasics/Ex3.java
@@ -0,0 +1,9 @@
+package com.wipro.languagebasics;
+
+public class Ex3 {
+   public static void main(String[] args) {
+	   int n1=Integer.parseInt(args[0]);
+	   int n2=Integer.parseInt(args[1]);
+	   System.out.println("The sum of numbers is "+(n1+n2));
+   }
+}
