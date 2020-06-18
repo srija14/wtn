94ba2c2d5567b7c9e885af4a99171a4f6fd8fe31
Administrator <Administrator@WIN-P8CJ2BN5V2H>
2020-06-18 23:40:47 +0530

    commit

---
+++ prog1/src/com/wipro/languagebasics/Ex4.java
@@ -0,0 +1,16 @@
+package com.wipro.languagebasics;
+
+public class Ex4 {
+     public static void main(String[] args) {
+    	 int n=Integer.parseInt(args[0]);
+    	 if(n>0) {
+    		 System.out.println("positive");
+    	 }
+    	 else if(n<0) {
+    		 System.out.println("negative");
+    	 }
+    	 else {
+    		 System.out.println("zero");
+    	 }
+     }
+}
