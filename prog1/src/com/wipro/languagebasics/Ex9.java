206146b01e7e63648e8ecae51fb914f96edb5d73
Administrator <Administrator@WIN-P8CJ2BN5V2H>
2020-06-26 07:38:12 +0530

    commit

---
+++ prog1/src/com/wipro/languagebasics/Ex9.java
@@ -0,0 +1,18 @@
+package com.wipro.languagebasics;
+
+public class Ex9 {
+	    public static void main (String[] args)
+	    {
+	        char input_char=args[0].charAt(0);
+		if ((input_char >= 65 && input_char <= 90) 
+	            || (input_char >= 97 && input_char <= 122)) 
+	            System.out.println(" Alphabet "); 
+	        else if (input_char >= 48 && input_char <= 57) 
+	            System.out.println(" Digit "); 
+	        else
+	            System.out.println(" Special Character ");
+	        
+	    }
+	    }
+
+
