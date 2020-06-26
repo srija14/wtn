41eeb4084c5889ed4cbd0b044ccc2acf8fefa552
Administrator <Administrator@WIN-P8CJ2BN5V2H>
2020-06-26 07:46:40 +0530

    commit

---
+++ prog1/src/com/wipro/languagebasics/Ex11.java
@@ -0,0 +1,16 @@
+package com.wipro.languagebasics;
+
+public class Ex11 {
+	public static void main(String args[])
+	{
+	char c1 = args[0].charAt(0);
+	if(Character.isLowerCase(c1))
+	{
+	System.out.println(Character.toUpperCase(c1));
+	}
+	else if(Character.isUpperCase(c1))
+	{
+	System.out.println(Character.toLowerCase(c1));
+	}
+	}
+	}
