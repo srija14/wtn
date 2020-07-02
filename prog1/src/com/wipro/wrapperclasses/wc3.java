3e708c389f0388d0c65986a2164dbb7b66f2559f
Administrator <Administrator@WIN-P8CJ2BN5V2H>
2020-07-02 23:38:28 +0530

    wrapperclass

---
+++ prog1/src/com/wipro/wrapperclasses/wc3.java
@@ -0,0 +1,20 @@
+package com.wipro.wrapperclasses;
+
+import java.util.Scanner;
+
+public class wc3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		String output = String.format("%8s", Integer.toBinaryString(input)).replace(' ', '0');
+		System.out.println(output);
+		
+		
+		sc.close();
+
+	}
+
+}
